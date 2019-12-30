63e2db4e27e6968f23ce980386aa91b60e8194bb
Robin Bisht <robinbisht7@gmail.com>
2019-12-30 21:29:40 +0530

    Adding Registration and Logging of users

--- merg/index.js
+++ merg/index.js
@@ -3,32 +3,9 @@ const gql  = require('graphql-tag');
 const mongoose = require('mongoose');
 
 const {MONGODB} = require('./config.js')
-const Post = require('./models/Post')
+const resolvers = require('./graphql/resolvers')
+const typeDefs = require('./graphql/typeDefs')
 
-const typeDefs = gql`
-type Post{
-    id: ID!
-    body: String!
-    createdAt: String!
-    username:String!
-}
-    type Query{
-        getPosts: [Post]
-    }
-`
-
-const resolvers = {
-    Query: {
-        async getPosts(){
-            try{
-                const posts = await Post.find();
-                return posts
-            } catch(err){
-                throw new Error(err);
-            }
-        } 
-    }
-}
 
 const server = new ApolloServer({
     typeDefs,
@@ -42,16 +19,5 @@ const Conn = async () => {
     console.log(`
         server running at ${res.url}`)
 }
-
-// mongoose
-//     .connect(MONGODB,{useNewUrlParser:true,useUnifiedTopology:true})
-//     .then(()=>{
-//         console.log('mongodb conencted')
-//         return server.listen({port:5000})
-//     })
-//     .then(res=>{
-//         console.log(`
-//         server running at ${res.url}`)
-//     })
 
 Conn()
