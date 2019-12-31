e525f5b3c78c511511fcacb06f987cd407bc28de
Robin Bisht <robinbisht7@gmail.com>
2019-12-31 19:05:09 +0530

    Adding 'Post body not be empty' authentication

--- merg/graphql/resolvers/posts.js
+++ merg/graphql/resolvers/posts.js
@@ -30,6 +30,11 @@ module.exports = {
     Mutation: {
         async createPost(_,{body},context){
             const user = checkAuth(context);
+
+            if(args.body.trim() === ''){
+                throw new Error('Post body must no tbe empty')
+            }
+
             console.log(user)
             const newPost = new Post({
                 body,
