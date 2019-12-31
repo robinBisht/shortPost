087b3020f2a48aff47f047fd7f6203df3ab4503e
Robin Bisht <robinbisht7@gmail.com>
2019-12-31 19:01:17 +0530

    Adding Likes and Comments feature

--- merg/graphql/resolvers/index.js
+++ merg/graphql/resolvers/index.js
@@ -1,12 +1,19 @@
 const postsResolvers = require('./posts')
 const usersResolvers = require('./users')
+const commentsResolver = require('./comments')
+
 
 module.exports = {
+    Post:{
+        likeCount: (parent)=>parent.likes.length,
+        commentCount: (parent)=> parent.comments.length
+    },
     Query: {
         ...postsResolvers.Query
     },
     Mutation: {
         ...usersResolvers.Mutation,
-        ...postsResolvers.Mutation
+        ...postsResolvers.Mutation,
+        ...commentsResolver.Mutation
     }
 }
--- merg/graphql/typeDefs.js
+++ merg/graphql/typeDefs.js
@@ -6,7 +6,23 @@ type Post{
     body: String!
     createdAt: String!
     username:String!
+    comments:[Comment]!
+    likes:[Like]!
+    likeCount:Int!
+    commentCount:Int!
 }
+type Comment{
+    id: ID!
+    createdAt:String!
+    username:String!
+    body:String!
+}
+type Like{
+    id:ID!
+    createdAt:String!
+    username:String!
+}
+
 type User{
     id:ID!
     email:String!
@@ -31,6 +47,9 @@ type Mutation{
     login(username:String!,password:String!):User!
     createPost(body: String!): Post!
     deletePost(postId: ID!):String!
+    createComment(postId:ID!, body:String!):Post!
+    deleteComment(postId:ID!,commentId: ID!):Post!
+    likePost(postId:ID!):Post!
 }
 
 `;
